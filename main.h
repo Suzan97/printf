54c2401c0e3fe5646c1a21713769d720dc2c39f8
Suzan97 <susannafula.sw@gmail.com>
2022-07-11 12:59:01 -0700

    produces output according to a format

--- main.h
+++ main.h
@@ -14,9 +14,9 @@
  */
 typedef struct flags
 {
-	int plus;
-	int space;
-	int hash;
+        int plus;
+        int space;
+        int hash;
 } flags_t;
 
 /**
@@ -29,8 +29,8 @@ typedef struct flags
 
 typedef struct printHandler
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
+        char c;
+        int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
@@ -57,6 +57,5 @@ int get_flag(char s, flags_t *f);
 /* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
-
 
 #endif
